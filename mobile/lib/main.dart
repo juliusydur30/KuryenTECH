190a88876093fd4f0ebaad193ef4659683010d9b
Mavsyyy <92443880+Mavsyyy@users.noreply.github.com>
2025-11-13 21:19:18 +0800

    Migrate from Supabase to Firebase for authentication

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,36 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'screens/auth/login_page.dart';
-import 'screens/home/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'auth_wrapper.dart';
+import 'services/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  await Supabase.initialize(
-    url: 'https://swnzgvpbakecusuzeanj.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN3bnpndnBiYWtlY3VzdXplYW5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NDkxNjAsImV4cCI6MjA3ODUyNTE2MH0.2Cr_Sv3ydv2RtVLQqxldcNxy81AWxpFIK4KRDoVEZhg',
-  );
-
-  runApp(const MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MainApp());
 }
 
-final supabase = Supabase.instance.client;
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'KuryenTECH',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-        scaffoldBackgroundColor: const Color(0xFF2C2C2C),
-      ),
-      home: supabase.auth.currentSession != null
-          ? const HomePage()
-          : const LoginPage(),
+      home: const AuthWrapper(),
     );
   }
 }
